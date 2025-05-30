0338ebfb806ad23ca58a5aa21ec49ea111475b35
Nathalia Gabriela Bortotti <126833977+nathiyy@users.noreply.github.com>
2025-05-29 23:25:47 -0300

    Update index.php

--- payments/index.php
+++ payments/index.php
@@ -1,19 +1,20 @@
 <?php
 
-$request_uri = $_SERVER['REQUEST_URI'];
+$request_uri = $_SERVER['REQUEST_URI']; // Pego a URL que foi acessada
 
 if ($request_uri === '/payment') {
+    // Faço uma requisição pra API de pedidos pra pegar os dados do pedido
     $orderJson = file_get_contents('http://orders:3002/order');
-    $orderData = json_decode($orderJson, true);
+    $orderData = json_decode($orderJson, true); // transformo o JSON em array
 
-    $response = [
+    $response = [  // Simulo um pagamento e retorno a resposta com status "paid"
         'status' => 'paid',
         'order' => $orderData
     ];
 
-    header('Content-Type: application/json');
+    header('Content-Type: application/json'); // Defino o tipo de retorno como JSON
     echo json_encode($response);
 } else {
-    http_response_code(404);
+    http_response_code(404); // Se não for a rota certa, retorno erro 404
     echo json_encode(['error' => 'Not found']);
 }
